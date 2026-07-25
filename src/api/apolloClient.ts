f8802252f4ee6e3dfeee67e5dd7b8320d372ff88
Rox_y <neroxxl128@gmail.com>
2026-07-26 03:00:11 +0400

    delete useless

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -1,13 +1,13 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri: 'https://api.github.com/graphql',
-        headers: {
-            authorization: 'Bearer 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',
-        },
-    }),
-    cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://api.github.com/graphql",
+    headers: {
+      authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  }),
+  cache: new InMemoryCache(),
 });
 
 export default client;
